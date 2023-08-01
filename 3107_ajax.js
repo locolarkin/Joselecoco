ef3c6de9e2d1da3538d833f139c8ca897ff4c89d
Hose Lecoco <joselecoco@gmail.com>
2023-08-01 19:34:24 +0100

    nooooope

---
+++ 3107_ajax.js
@@ -0,0 +1,29 @@
+//Metodo Fetch
+//Realizar peticiones AJAX de una manera mas simple de como se hacia antes.
+
+var div_usuarios =document.querySelector('#usuarios');
+var usuarios =[];
+
+fetch('https://reqres.in/api/users?page=2')
+.then(data => data.json())
+.then(user =>{
+    usuarios = user.data;
+    console.log(usuarios);
+
+    usuarios.map((user, i)=>{
+        let nombre= document.createElement('h3');
+        nombre.innerHTML = (i + 1)+ " " + user.first_name + " " + user.last_name
+
+        div_usuarios.append(nombre);
+
+        document.querySelector('.loading').style.display ='none';
+    })
+});
+
+try{
+    var year = 2023;
+    alert(year);
+}catch(error){
+    console.log(error);
+    alert("Ha ocurrido un error en el código");
+}
